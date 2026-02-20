946fb1f4113e7f75f7091659c417f259d1054b1a
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2026-02-20 09:02:13 +0100

    Hello world program

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+
+from backend.main import app
+
+
+client = TestClient(app)
+
+
+def test_read_root_returns_hello_world() -> None:
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, World!"}
